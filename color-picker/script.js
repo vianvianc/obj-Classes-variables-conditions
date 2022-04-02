ffed2ca099506646ce5d87e2f58a37f96b834529
Vian Charbonneau <vian_charbonneau@yahoo.com>
2022-04-02 17:19:46 -0600

    commit

---
+++ color-picker/script.js
@@ -0,0 +1,32 @@
+window.onload = function () {
+    initColorPicker();
+};
+
+function initColofrPicker() {
+    let colorBox = document.getElementById("color-box");
+    let rgb = {
+        red: document.getElementById("red"),
+        green: document.getElementById("green"),
+        blue: document.getElementById("blue")
+    };
+    setColorPiickerEventListeners(colorBox, rgb);
+}
+
+function setColorPickerEventListeners(colorBox, rgb) {
+    rgb.red.addEventListener('change', () => {
+        console.log("Red value: ", rgb.red.value);
+        setBoxBGColor(colorBox, 150, 150, 150);
+    });
+    rgb.green.addEventListener('change', () => {
+        console.log("Green value: ", rgb.green.value);
+    });
+    rgb.blue.addEventListener('change', () => {
+        console.log("Blue value: ", rgb.blue.value);
+    });
+}
+
+function setBoxBGColor(colorBox, red, green, blue) {
+    rgbVal = [red, green, blue].join(',');
+    colorBox.style.backgroundColor = "rgb(" + rgbVal_+ ")";
+
+}
